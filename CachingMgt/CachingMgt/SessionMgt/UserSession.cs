13044db9321f42fe096f26ec09c7a353db9f5ed9
chandan singh <chandanyara@gmail.com>
2019-06-03 01:10:47 +0530

    Basic setup completed for caching. Dependency resolver implemeted .

---
+++ CachingMgt/CachingMgt/SessionMgt/UserSession.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CachingMgt.Models;
+using CachingMgt.Utility;
+
+namespace CachingMgt.SessionMgt
+{
+    public class UserSession : IUserSession
+    {
+        private const string UserInfoKey = "userInfo";
+        private HttpSessionStateBase HttpSessionStateBase { get; set; }
+        public UserSession(IHttpContextFactory httpContextFactory)
+        {
+            HttpSessionStateBase = httpContextFactory.GetHttpSessionStateBase();
+        }
+
+        /// <summary>
+        /// Get User session data based on key.
+        /// </summary>
+        /// <returns></returns>
+        public UserInfo GetUserInfo()
+        {
+         return HttpSessionStateBase.GetSessionData<UserInfo>(UserInfoKey);
+        }
+
+        /// <summary>
+        /// Set user information into session with specified key.
+        /// </summary>
+        /// <param name="userInfo"></param>
+        public void SetUserInfo(UserInfo userInfo)
+        {
+            HttpSessionStateBase.SetSessionData<UserInfo>(UserInfoKey,userInfo);
+        }
+    }
+}
---
+++ CachingMgt/CachingMgt/Utility/SessionExtension.cs
@@ -0,0 +1,49 @@
+﻿
+namespace CachingMgt.Utility
+{
+    using CachingMgt.CustomException;
+    using System;
+    using System.Web;
+    public static class SessionExtension
+    {
+        /// <summary>
+        /// Set session state for given data for particular selected model information.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sessionStateBase"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void SetSessionData<T>(this HttpSessionStateBase sessionStateBase, string key, object value)
+        {
+            try
+            {
+                sessionStateBase[key] = (T)value;
+            }
+            catch (Exception ex)
+            {
+                throw new SessionException($"An error occured when retrieving the session data for {key}", ex);
+            }
+        }
+
+        /// <summary>
+        /// Get Session data for given key.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sessionStateBase"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+
+        public static T GetSessionData<T>(this HttpSessionStateBase sessionStateBase, string key)
+        {
+            try
+            {
+                return (T)sessionStateBase[key];
+            }
+            catch (Exception ex)
+            {
+                throw new SessionException($"An error occured when retrieving the session data for {key}", ex);
+            }
+        }
+
+    }
+}
