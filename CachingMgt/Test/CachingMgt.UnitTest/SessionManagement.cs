5088165d1f0d3a80b67bbaf17aa3b48dc9e3e859
chandan singh <chandanyara@gmail.com>
2019-06-03 18:51:02 +0530

    Moq framework added.

---
+++ CachingMgt/Test/CachingMgt.UnitTest/SessionManagement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+namespace CachingMgt.UnitTest
+{
+    [TestClass]
+    public class SessionManagement
+    {
+        [TestMethod]
+        public void SessionManagement_About()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.AreEqual("","");
+        }
+
+       
+    }
+}
