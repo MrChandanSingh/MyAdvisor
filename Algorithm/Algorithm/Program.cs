57fd26e31e523a975a4284420ba0549ad5265128
chandan singh <chandanyara@gmail.com>
2019-06-03 19:43:23 +0530

    check palindrome string

--- Algorithm/Algorithm/Program.cs
+++ Algorithm/Algorithm/Program.cs
@@ -10,14 +10,68 @@ namespace Algorithm
             #region Get fibonacci series using generic collection.
             var number = 30;
             IEnumerable<int> fibonacciSeries = GetFibonacciSeries(number);
-            foreach(var fibNumber in fibonacciSeries)
+            foreach (var fibNumber in fibonacciSeries)
             {
                 Console.Write($"{fibNumber} ");
             }
+           
+            #endregion
+            #region check for palindrome string.
+            var phrase = "example";
+            bool isPalindrome = CheckPhraseIspalindrome(phrase);
+            Console.WriteLine($"Is the given phrase palindrome: {isPalindrome}");
             Console.ReadLine();
             #endregion
         }
+       
+        //The below code has some logical issue, working on it's fixes. 
+        private static bool CheckPhraseIspalindrome(string phrase)
+        {
+            try
+            {
+                var oddCount = 0;
+                var table = new int[(int)(Char.GetNumericValue('z') - Char.GetNumericValue('a') + 1)];
+                foreach(var charItem in phrase.ToCharArray())
+                {
+                    var x = getCharacterNumber(charItem);
+                    if (x != -1)
+                    {
+                        table[x]++;
+                        if (table[x] % 2 == 1)
+                        {
+                            oddCount++;
+                        }
+                        else
+                        {
+                            oddCount--;
+                        }
+                    }
+                }
+                return oddCount <= 1;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception();
+            }
+        }
 
+        private static int getCharacterNumber(char charItem)
+        {
+            var a = (int)Char.GetNumericValue('a');
+            var z = (int)Char.GetNumericValue('z');
+            var value = (int)Char.GetNumericValue(charItem);
+            if(a<=value && value <= z)
+            {
+                return value - a;
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Get fibonacci number series.
+        /// </summary>
+        /// <param name="number">Current given number.</param>
+        /// <returns></returns>
         private static IEnumerable<int> GetFibonacciSeries(int number)
         {
             var prev = -1;
