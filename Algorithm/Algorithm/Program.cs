a1246a30cd0c3a00e088c27c8afd2fabdd748e27
chandan singh <chandanyara@gmail.com>
2019-06-03 19:17:10 +0530

    Fibonacci series implementation

---
+++ Algorithm/Algorithm/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Algorithm
+{
+    using System;
+    using System.Collections.Generic;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Get fibonacci series using generic collection.
+            var number = 30;
+            IEnumerable<int> fibonacciSeries = GetFibonacciSeries(number);
+            foreach(var fibNumber in fibonacciSeries)
+            {
+                Console.Write($"{fibNumber} ");
+            }
+            Console.ReadLine();
+            #endregion
+        }
+
+        private static IEnumerable<int> GetFibonacciSeries(int number)
+        {
+            var prev = -1;
+            int next = 1;
+            for (var i = 0; i < number; i++)
+            {
+                var sum = prev + next;
+                prev = next;
+                next = sum;
+                //using defered execution concept.
+                yield return sum;
+            }
+        }
+    }
+}
